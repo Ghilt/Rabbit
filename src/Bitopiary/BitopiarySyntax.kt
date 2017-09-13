b984ed0c2484841db940621ba7695e1eb3721a00
Adam Nilsson <adam.n.a3@gmail.com>
2017-09-13 16:50:33 +0200

    Add some resources

--- src/Bitopiary/BitopiarySyntax.kt
+++ src/Bitopiary/BitopiarySyntax.kt
@@ -220,7 +220,7 @@ enum class OperatorType {
         override val instructionConstructor = ::StartFunctionInstruction
     },
     CHARACTER {
-        /**Character is the default character*/
+        /**Character is the default instruction and as such is not limited to the ones listed below: the list is deprecated*/
         override val toCharacter = charArrayOf(
                 '0','1','2','3','4','5','6','7','8','9',
                 'A','B','C','D','E','F','G','H','I','J',

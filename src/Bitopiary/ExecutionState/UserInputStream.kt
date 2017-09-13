dcd22a6c6734cfd2e31679b9c3f8a09ec1c5807d
Adam Nilsson <adam.n.a3@gmail.com>
2017-09-13 14:03:38 +0200

    Fix bug with UserInputStream.kt

--- src/Bitopiary/ExecutionState/UserInputStream.kt
+++ src/Bitopiary/ExecutionState/UserInputStream.kt
@@ -13,12 +13,19 @@ class UserInputStream{
     }
 
     fun get(): Int {
+        var characterFound = false
         val listOfDigits = data.fold(ArrayList<Char>()) {
-            accumulator, c -> if (c.isDigit()) accumulator.apply{add(c)} else accumulator }
-
+            accumulator, c -> if (c.isDigit() && !characterFound) {
+                                    accumulator.apply {add(c)}
+                                } else {
+                                    characterFound = true
+                                    accumulator
+                                }
+        }
         return if (listOfDigits.isEmpty() && data.isNotEmpty()){
             data.removeAt(0).toInt()
         } else {
+            listOfDigits.forEach { data.removeAt(0)}
             listOfDigits.toInt()
         }
     }
